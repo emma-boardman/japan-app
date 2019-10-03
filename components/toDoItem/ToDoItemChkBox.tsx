5bb50b49328b7ba8033e6dfa778ee515badf9180
E2 <e2@macbook-pro-5.home>
2019-10-03 07:46:16 +0100

    begin experimenting with icon libraries

--- components/toDoItem/ToDoItemChkBox.tsx
+++ components/toDoItem/ToDoItemChkBox.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableNativeFeedback } from "react-native";
 import Colors from "../../constants/Colors";
 import Svg, { Path } from "react-native-svg";
+import { Ionicons } from "@expo/vector-icons";
 
 const ToDoItemChkBox = props => {
   return (
     <TouchableNativeFeedback onPress={props.onValueChange}>
       <View style={styles.container}>
-        {props.value && <Text>X</Text>
+        {props.value &&  <Ionicons name={"md-checkmark"} size={23} color={Colors.primaryBlack} />
 
         //   <Svg viewBox="0 0 15 15" width="15" height="15">
         //   <Path d="M640 192L256 576 128 448 0 576l256 256 512-512L640 192z" />
